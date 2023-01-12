c073f004a4d009cb5eb5605ff7c252ae5dad5026
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2023-01-12 05:53:19 +0000

    whitespaces

--- main.cs
+++ main.cs
@@ -11,7 +11,8 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-class Program {
+class Program
+    {
     public static async Task Main()
     {
         HttpClient client = new HttpClient();
@@ -19,7 +20,7 @@ class Program {
             "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
         );
         // Console.WriteLine(response);
-        var jsonAsDictionary  = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
         // Console.WriteLine(jsonAsDictionary);
         Console.WriteLine("");
         JsonNode forecastNode = JsonNode.Parse(response)!;
